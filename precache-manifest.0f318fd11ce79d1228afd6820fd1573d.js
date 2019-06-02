29e4ea99463f7237fbc5090d5928af1959c0f5c5
Valefar Frontend Developer <inzhener.intellect@gmail.com>
2019-06-02 20:18:40 +0700

    Add files via upload

---
+++ precache-manifest.0f318fd11ce79d1228afd6820fd1573d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3929a0c6b43c99795af5dd149850bb",
+    "url": "/ReactFood2Fork/index.html"
+  },
+  {
+    "revision": "b539c9c542abfdfe5e8e",
+    "url": "/ReactFood2Fork/static/css/main.1cf93ebd.chunk.css"
+  },
+  {
+    "revision": "e083292f352b8a48b197",
+    "url": "/ReactFood2Fork/static/js/2.61ebbd48.chunk.js"
+  },
+  {
+    "revision": "b539c9c542abfdfe5e8e",
+    "url": "/ReactFood2Fork/static/js/main.fdac8648.chunk.js"
+  },
+  {
+    "revision": "275bfd2b2eb3dc279de7",
+    "url": "/ReactFood2Fork/static/js/runtime~main.27d164cd.js"
+  }
+]);
